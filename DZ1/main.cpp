fd7d17b5179694ba6262acd3d90f7cb37be1c217
PoryazV <m2302173@edu.misis.ru>
2024-02-12 15:31:46 +0300

    modified:   DZ1/main.cpp deleted:    DZ1/rectangle.cpp modified:   DZ1/rectangle.hpp

--- DZ1/main.cpp
+++ DZ1/main.cpp
@@ -4,12 +4,17 @@
 int main()
 {
     Rectangle r;
-    std::cout << "Default rect Area: " << r.area() << std::endl;
     std::cout << "Default rect Length: " << r.length() << std::endl;
+    std::cout << "Default rect Width: " << r.width() << std::endl;
+    std::cout << "Default rect Area: " << r.area() << std::endl;
+    std::cout << "Default rect Edge length: " << r.edgeLength() << std::endl;
     double ax, ay, bx, by;
+    std::cout << "Enter: Ax, Ay, Bx, By" << std::endl;
     std::cin >> ax >> ay >> bx >> by;
     r.setA(ax, ay);
     r.setB(bx, by);
-    std::cout << "Given rect Area: " << r.area() << std::endl;
     std::cout << "Given rect Length: " << r.length() << std::endl;
+    std::cout << "Given rect Width: " << r.width() << std::endl;
+    std::cout << "Given rect Area: " << r.area() << std::endl;
+    std::cout << "Given rect Edge length: " << r.edgeLength() << std::endl;
 }
--- DZ1/rectangle.hpp
+++ DZ1/rectangle.hpp
@@ -6,18 +6,36 @@ class Rectangle
 public:
     Rectangle() = default;
 
-    Rectangle(double _ax, double _ay, double _bx, double _by);
+    Rectangle(double _ax, double _ay, double _bx, double _by){
+        ax = _ax;
+        ay = _ay;
+        bx = _bx;
+        by = _by;
+    }
+    void setA(double _ax, double _ay){ax = _ax; ay = _ay;}
+    void setB(double _bx, double _by){bx = _bx; by = _by;}
 
-    void setA(double _ax, double _ay);
-    void setB(double _bx, double _by);
+    double getAx(){return ax;}
+    double getAy(){return ay;}
+    double getBx(){return bx;}
+    double getBy(){return by;}
 
-    double getAx();
-    double getAy();
-    double getBx();
-    double getBy();
-
-    double area();
-    double length();
+    double length()
+    {
+        return abs(by-ay);
+    }
+    double width()
+    {
+        return abs(bx-ax);
+    }
+    double area()
+    {
+        return length() * width();
+    }
+    double edgeLength()
+    {
+        return length()*2 + width()*2;
+    }
 
 private:
     double ax{0.0}, ay{0.0};
